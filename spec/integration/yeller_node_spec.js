45977ff5bdc57fc591300e0e6eebb182c1273627
Tom Crayford <tcrayford@googlemail.com>
2014-11-30 14:39:23 +0000

    implement error handling logic

--- spec/integration/yeller_node_spec.js
+++ spec/integration/yeller_node_spec.js
@@ -2,6 +2,10 @@ var yeller = require('../../yeller_node');
 var nock = require('nock');
 
 describe('yeller', function () {
+  afterEach(function () {
+    nock.cleanAll();
+  });
+
   it("reports an error to yeller's api", function (done) {
     var api = nock('https://collector1.yellerapp.com')
                     .post('/API_TOKEN')
@@ -13,6 +17,104 @@ describe('yeller', function () {
     } catch (e) {
       client.report(e, function () {
         api.done();
+        done();
+      });
+    }
+  });
+
+  it("submits an error to multiple endpoints", function (done) {
+    var api1 = nock('https://collector1.yellerapp.com')
+                    .post('/API_TOKEN')
+                    .reply(200, 'success');
+    var api2 = nock('https://collector2.yellerapp.com')
+                    .post('/API_TOKEN')
+                    .reply(200, 'success');
+    nock.disableNetConnect();
+    var client = yeller.client({token: 'API_TOKEN'});
+    try {
+      throw new Error('test error');
+    } catch (e) {
+      client.report(e, function () {
+        api1.done();
+        client.report(e, function () {
+          api2.done();
+          done();
+        });
+      });
+    }
+  });
+
+  it("submits an error again when the first endpoint fails", function (done) {
+    var api1 = nock('https://collector1.yellerapp.com')
+                    .post('/API_TOKEN')
+                    .reply(500, 'THE BEES ARE IN MY EYES');
+    var api2 = nock('https://collector2.yellerapp.com')
+                    .post('/API_TOKEN')
+                    .reply(200, 'success');
+    nock.disableNetConnect();
+    var client = yeller.client({token: 'API_TOKEN'});
+    try {
+      throw new Error('test error');
+    } catch (e) {
+      client.report(e, function () {
+        api1.done();
+        api2.done();
+        done();
+      });
+    }
+  });
+
+  it("resubmits errors until an api succeeds", function (done) {
+    var api1 = nock('https://collector1.yellerapp.com')
+                    .post('/API_TOKEN')
+                    .reply(500, 'THE BEES ARE IN MY EYES');
+    var api2 = nock('https://collector2.yellerapp.com')
+                    .post('/API_TOKEN')
+                    .reply(500, 'THE BEES ARE IN MY EYES');
+    var api3 = nock('https://collector3.yellerapp.com')
+                    .post('/API_TOKEN')
+                    .reply(500, 'THE BEES ARE IN MY EYES');
+    var api4 = nock('https://collector4.yellerapp.com')
+                    .post('/API_TOKEN')
+                    .reply(500, 'THE BEES ARE IN MY EYES');
+    var api5 = nock('https://collector5.yellerapp.com')
+                    .post('/API_TOKEN')
+                    .reply(200, 'success');
+    nock.disableNetConnect();
+    var client = yeller.client({token: 'API_TOKEN'});
+    try {
+      throw new Error('test error');
+    } catch (e) {
+      client.report(e, function () {
+        api1.done();
+        api2.done();
+        api3.done();
+        api4.done();
+        api5.done();
+        done();
+      });
+    }
+  });
+
+  it("stops resubmitting when it's done too many retries", function (done) {
+    var api1 = nock('https://collector1.yellerapp.com')
+                    .post('/API_TOKEN')
+                    .reply(500, 'THE BEES ARE IN MY EYES');
+    var api2 = nock('https://collector1.yellerapp.com')
+                    .post('/API_TOKEN')
+                    .reply(500, 'THE BEES ARE IN MY EYES');
+    nock.disableNetConnect();
+    var client = yeller.client({
+        token: 'API_TOKEN',
+        //TODO: this shouldn't be called clients
+        clients: ['collector1.yellerapp.com']
+    });
+    try {
+      throw new Error('test error');
+    } catch (e) {
+      client.report(e, function () {
+        api1.done();
+        api2.done();
         done();
       });
     }
--- yeller_node.js
+++ yeller_node.js
@@ -1,24 +1,56 @@
 var https = require('https');
 
-var YellerClient = function (token) {
+var DEFAULT_CLIENTS = [
+  'collector1.yellerapp.com',
+  'collector2.yellerapp.com',
+  'collector3.yellerapp.com',
+  'collector4.yellerapp.com',
+  'collector5.yellerapp.com',
+];
+
+
+var YellerClient = function (token, clients) {
   this.token = token;
+  this.clients = clients;
+  this.maxRetryCount = this.clients.length * 2;
 };
 
-YellerClient.prototype.report = function(error, callback) {
-  var yellerCallback = function () {
-    callback();
+YellerClient.prototype.rotateClients = function () {
+  var lastClient = this.clients.shift();
+  this.clients.push(lastClient);
+};
+
+YellerClient.prototype.reportAndHandleRetries = function (error, currentRequestCount, callback) {
+  var that = this;
+
+  var yellerCallback = function (res) {
+    that.rotateClients();
+    if (res.statusCode === 200) {
+      callback();
+    } else if (currentRequestCount < that.maxRetryCount)  {
+      that.reportAndHandleRetries(error, currentRequestCount + 1, callback);
+    } else {
+      callback(res);
+    }
   };
   var req = https.request({
-    host: 'collector1.yellerapp.com',
+    host: this.clients[0],
     path: '/' + this.token,
     method: 'POST'
   },
   yellerCallback);
+  // handle request errors
+  // handle request timeouts
   req.end();
 };
 
+YellerClient.prototype.report = function(error, callback) {
+  this.reportAndHandleRetries(error, 1, callback);
+};
+
 var client = function(opts) {
-  return new YellerClient(opts.token);
+  var clients = opts.clients || DEFAULT_CLIENTS.slice(0);
+  return new YellerClient(opts.token, clients);
 };
 
 module.exports = {
